a38eb958572356872b8f49bc55dbaf39588999b5
Coin Graham <coingraham@user.noreply.github.com>
2019-11-25 16:28:41 -0500

    Day 11 updated complete using sum tables

--- 2018/day11.py
+++ 2018/day11.py
@@ -1,47 +1,48 @@
 import math
-
-
-test1 = 8
-test2 = 18
-test3 = 42
-
-real = 2694
-
-
-def get_power(x, y, serial):
-    rack_id = x + 10
-    power_level = rack_id * y
-    add_serial = power_level + serial
-    multiply_by_rid = add_serial * rack_id
-    hundreds = (multiply_by_rid // 100) % 10
-    return hundreds - 5
-
-
-def build_fuel_cells(serial):
-    plane = [[0 for j in range(0, 300)] for i in range(0, 300)]
-
-    for y in range(0, 300):
-        for x in range(0, 300):
-            plane[y][x] = get_power(x, y, serial)
-
-    values = 0
-    largest_point = []
-    # for size in range(1, 301):
-    size = 16
-    for y in range(0, 301 - size):
-        for x in range(0, 301 - size):
-            value = 0
-            for i in range(size):
-                for j in range(size):
-                    value += plane[x + i][y + j]
-                    if value > values:
-                        values = value
-                        largest_point = [y, x, size]
-
-    print(largest_point)
+import numpy
 
 
 if __name__ == '__main__':
-    build_fuel_cells(real)
-    # [243, 38]
-    # 230, 142, 18
+
+    test1 = 8
+    test2 = 18
+    test3 = 42
+    real = 2694
+
+    serial = int(real)
+
+    def get_power(x, y):
+        rack_id = (x + 1) + 10
+        power_level = rack_id * (y + 1)
+        add_serial = power_level + serial
+        multiply_by_rid = add_serial * rack_id
+        hundreds = (multiply_by_rid // 100 % 10) - 5
+        return hundreds
+
+
+    grid = numpy.fromfunction(get_power, (300, 300))
+
+    integral = grid.cumsum(axis=0).cumsum(axis=1)
+    integral = integral.astype(int)
+
+
+    def check_window(x, y, width):
+        offset = width - 1
+        x_offset = x + offset
+        y_offset = y + offset
+        total = integral[x_offset][y_offset]
+        top = integral[x_offset][y - 1] if y > 0 else 0
+        left = integral[x - 1][y_offset] if x > 0 else 0
+        overlap = integral[x - 1][y - 1] if x > 0 and y > 0 else 0
+        return total - top - left + overlap
+
+
+    for width in range(3, 300):
+        # windows = numpy.fromfunction(check_window, (300 - width + 1, 300 - width + 1), int, width=width) # can't figure out how to make this work
+        windows = numpy.zeros((300 - width + 1, 300 - width + 1), int)
+        for i in range(300 - width + 1):
+            for j in range(300 - width + 1):
+                windows[i][j] = check_window(i, j, width)
+        maximum = int(windows.max())
+        location = numpy.where(windows == maximum)
+        print(width, maximum, location[0][0] + 1, location[1][0] + 1)
