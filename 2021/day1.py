b7a3c0ab800072a3bb07fefee079937d531770ae
Coin Graham <coin.graham@cleardata.com>
2021-12-01 09:08:51 -0500

    adding day 1

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,4 +1,54 @@
+from typing import Mapping
 from aocd.models import Puzzle
 puzzle = Puzzle(year=2021, day=1)
 
-print(puzzle.input_data)
+# print(puzzle.input_data)
+
+question_input = puzzle.input_data
+
+sample_data = '''
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263'''
+
+question_input = [int(n) for n in question_input.split()]
+
+
+def part_one():
+    increased = 0
+    for index, item in enumerate(question_input):
+        if index == 0:
+            continue
+
+        if item > question_input[index - 1]:
+            increased += 1
+
+    return increased
+
+
+def part_two():
+    increased = 0
+    previous_sum = 0
+    for index, item in enumerate(question_input):
+        if index > 3:
+            previous_sum += item
+
+        new_sum = item + previous_sum - question_input[index - 3]
+
+        if new_sum > previous_sum:
+            increased += 1
+
+    previous_sum = new_sum
+
+    return increased
+
+
+print(part_one)
+print(part_two)
