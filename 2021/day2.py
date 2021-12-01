2408004290fc4ca5a77533959b099a252b47e856
Coin Graham <coin.graham@cleardata.com>
2021-12-01 09:38:57 -0500

    setup for day 2

---
+++ 2021/day2.py
@@ -0,0 +1,24 @@
+from aocd.models import Puzzle
+puzzle = Puzzle(year=2021, day=2)
+
+# print(puzzle.input_data)
+
+question_input = puzzle.input_data
+
+sample_data = ''''''
+
+# question_input = [int(n) for n in sample_data.split()]
+# question_input = [int(n) for n in question_input.split()]
+
+
+def part_one():
+    pass
+
+
+def part_two():
+    pass
+
+
+if __name__ == '__main__':
+    print(part_one())
+    print(part_two())
