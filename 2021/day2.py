42123a23e028285ba1a95d7d63c91a4cd7b387e0
Coin Graham <coin.graham@cleardata.com>
2021-12-02 16:43:58 -0500

    finished day 2

--- 2021/day2.py
+++ 2021/day2.py
@@ -1,22 +1,59 @@
+import math
 from aocd.models import Puzzle
 puzzle = Puzzle(year=2021, day=2)
 
 # print(puzzle.input_data)
 
-question_input = puzzle.input_data
+sample_data = '''forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2'''
 
-sample_data = ''''''
-
-# question_input = [int(n) for n in sample_data.split()]
-# question_input = [int(n) for n in question_input.split()]
+# question_input = [item for item in sample_data.splitlines()]
+question_input = [item for item in puzzle.input_data.splitlines()]
 
 
 def part_one():
-    pass
+    position = complex(0, 0)
+    movement = {
+        "forward": complex(1, 0),
+        "backward": complex(-1, 0),
+        "down": complex(0, 1),
+        "up": complex(0, -1)
+    }
+
+    for line in question_input:
+        command, amount = line.split(" ")
+        position += (movement[command] * int(amount))
+    
+    return int(abs(position.real * position.imag))
 
 
 def part_two():
-    pass
+    position = complex(0, 0)
+    aim = 0
+    movement = {
+        "forward": complex(1, 0),
+        "backward": complex(-1, 0),
+        "down": complex(0, 1),
+        "up": complex(0, -1)
+    }
+
+    for line in question_input:
+        command, amount_text = line.split(" ")
+        amount = int(amount_text)
+        if command == "down":
+            aim += amount
+        if command == "up":
+            aim -= amount
+        if command == "forward":
+            position += complex(amount, 0)
+            position += complex(0, amount * aim)
+        
+    print(position)
+    return int(abs(position.real * position.imag))
 
 
 if __name__ == '__main__':
