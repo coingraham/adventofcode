cf887583b35da7114bc02445a4b59e9bf197d655
Coin Graham <coin.graham@cleardata.com>
2021-12-07 00:37:33 -0500

    finished day 6 queued up 7

--- 2021/day6.py
+++ 2021/day6.py
@@ -1,20 +1,66 @@
 from aocd.models import Puzzle
 puzzle = Puzzle(year=2021, day=6)
 
-sample_data = ''''''
+sample_data = '''3,4,3,1,2'''
 
-# question_input = [int(n) for n in sample_data.split()]
-# question_input = [int(n) for n in puzzle.input_data.split()]
-
-
-def part_one():
-    pass
+# question_input = [int(n) for n in sample_data.split(",")]
+question_input = [int(n) for n in puzzle.input_data.split(",")]
 
 
-def part_two():
-    pass
+def part_one(initial, day_limit):
+    next_day = []
+    add_fish = 0
+    days = 0
+    while True:
+        if days == day_limit:
+            break
+
+        for fish in initial:
+            if fish == 0:
+                next_day.append(6)
+                add_fish += 1
+            else:
+                next_day.append(fish - 1)
+        
+        days += 1
+        for n in range(add_fish):
+            next_day.append(8)
+        initial = next_day
+        next_day = []
+        add_fish = 0
+
+        if days % 7 == 0:
+            initial.sort()
+            print(initial)
+
+    return len(initial)
+        
+
+
+def part_two(initial, days_limit):
+    fish_lives = {
+        0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0,
+    }
+    for fish in initial:
+        fish_lives[fish] += 1
+    days = 0
+    hold = 0
+    while True:
+        if days == days_limit:
+            break
+
+        hold = fish_lives[0]
+        for item in range(8):
+            fish_lives[item] = fish_lives[item + 1]
+        
+        fish_lives[6] += hold
+        fish_lives[8] = hold
+        
+        days += 1
+
+    return sum(fish_lives.values())
 
 
 if __name__ == '__main__':
-    print(part_one())
-    print(part_two())
+    # print(part_one(question_input, 80))
+    print(part_two(question_input, 256))
---
+++ 2021/day7.py
@@ -0,0 +1,20 @@
+from aocd.models import Puzzle
+puzzle = Puzzle(year=2021, day=7)
+
+sample_data = ''''''
+
+# question_input = [int(n) for n in sample_data.split()]
+# question_input = [int(n) for n in puzzle.input_data.split()]
+
+
+def part_one():
+    pass
+
+
+def part_two():
+    pass
+
+
+if __name__ == '__main__':
+    print(part_one())
+    print(part_two())
