0aef5d6fbf0976051c097ae0a8b5b641209a1d1f
Coin Graham <coin.graham@bespinglobal.com>
2024-12-05 14:19:32 -0600

    finished day 5

---
+++ 2024/day5.py
@@ -0,0 +1,126 @@
+from aocd.models import Puzzle
+puzzle = Puzzle(year=2024, day=5)
+
+sample_data = '''47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47'''
+
+# q_i = [n for n in sample_data.splitlines()]
+q_i = [n for n in puzzle.input_data.splitlines()]
+
+before = {}
+after = {}
+updates = []
+non_compliant = []
+for line in q_i:
+    if "|" in line:
+        i, j = map(int, line.split('|'))
+
+        if i not in before:
+            before[i] = []
+
+        before[i].append(j)
+
+        if j not in after:
+            after[j] = []
+
+        after[j].append(i)
+
+    if "," in line:
+        updates.append([*map(int, line.split(","))])
+
+
+def check_values(update):
+    for i, item in enumerate(update):
+        # skip the first, it doesn't matter
+        if i == 0:
+            continue
+
+        # if you made it to the end your good
+        if i == len(update) - 1:
+            middle = i // 2
+            return update[middle]
+
+        # check middle, are its numbers before and after correct?
+        for follower in before.get(item, []):
+            if follower in update and update.index(follower) < i:
+                return False
+
+        for leaders in after.get(item, []):
+            if leaders in update and update.index(leaders) > i:
+                return False
+
+def part_one():
+    good_updates = []
+    for update in updates:
+        if middle := check_values(update):
+            good_updates.append(middle)
+        else:
+            non_compliant.append(update)
+
+    return sum(good_updates)
+
+
+def check_values_with_fix(update):
+    for i, item in enumerate(update):
+        if i == 0:
+            continue
+
+        if i == len(update) - 1:
+            middle = i // 2
+            return update[middle]
+
+        # check middle, are its numbers before and after correct?
+        for follower in before.get(item, []):
+            if follower in update and update.index(follower) < i:
+                update.insert(i, update.pop(update.index(follower)))
+                return update
+
+        for leader in after.get(item, []):
+            if leader in update and update.index(leader) > i:
+                update.insert(i, update.pop(update.index(leader)))
+                return update
+
+def part_two():
+    fixed_updates = []
+    for update in non_compliant:
+        while True:
+            result = check_values_with_fix(update)
+
+            if isinstance(result, int):
+                fixed_updates.append(result)
+                break
+
+            update = result
+
+    return sum(fixed_updates)
+
+
+if __name__ == '__main__':
+    print(part_one())
+    print(part_two())
